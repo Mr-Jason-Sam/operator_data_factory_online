6c2b2f52d733d51cddab3d16701a782978029c58
Mr-Jason-Sam <Mr.Jason_Sam@iCloud.com>
2022-01-07 10:48:18 +0800

    【Feat】 变更ifind安装路径

--- operator_data_factory/bin/environment/ifund_install.sh
+++ operator_data_factory/bin/environment/ifund_install.sh
@@ -5,10 +5,10 @@ cd ../..
 export BASE_DIR=$(pwd)
 
 ## oracle连接环境安装
-mkdir -p /opt/ifund
-cd /opt/ifund
+mkdir -p $BASE_DIR/tools/ifund
+cd $BASE_DIR/tools/ifund
 
 # 解压包
-tar -zxvf $BASE_DIR/resource/THSDataInterface_Linux_20210427.tar.gz
+tar -zxvf $BASE_DIR/resource/DataInterface_free_Linux_20210812.tar.gz
 
 sudo python3 bin64/installiFinDPy.py
